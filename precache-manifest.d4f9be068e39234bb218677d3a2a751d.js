0a7b6895d813e1dca589c77bd646402edc120e6d
Hemanth R H <hemanth.ramachandra@enparadigm.com>
2020-10-04 15:02:31 +0530

    Updates

---
+++ precache-manifest.d4f9be068e39234bb218677d3a2a751d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81ef46cea0c562b929b06b1553b4028",
+    "url": "/chatting_app/index.html"
+  },
+  {
+    "revision": "8d80ea076292915594ea",
+    "url": "/chatting_app/static/css/main.7d815ecc.chunk.css"
+  },
+  {
+    "revision": "b1057533b3be08f37db5",
+    "url": "/chatting_app/static/js/2.1573ab8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatting_app/static/js/2.1573ab8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d80ea076292915594ea",
+    "url": "/chatting_app/static/js/main.b2d0c332.chunk.js"
+  },
+  {
+    "revision": "7fa0d3220eb48dc5acf4",
+    "url": "/chatting_app/static/js/runtime-main.c408f46a.js"
+  }
+]);
